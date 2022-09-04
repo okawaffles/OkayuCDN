bde0d17fa993062820c8793528b6bc1ad83eec36
okawaffles <okawaffles@gmail.com>
2022-09-04 15:46:20 -0500

    oops forgot to update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var config = require('./config.json');
-var okayuLogger = require('./cs-modules/okayuLogger/index.js');
+const {info, error, warn} = require('okayulogger');
 
 okayuLogger.info("boot", `Starting OkayuCDN Server ${config.version}${config.buildType}`);
 okayuLogger.info("boot", `Server must be restarted to change config.\nAccount Creation: ${config.enableAccountCreation}\nUploading: ${config.enableUploading}\nAnonymous Uploading (not implemented): ${config.enableAnonymousUploading}`);
