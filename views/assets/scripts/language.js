64796fe1df1673acd07e4b7188c41c8b0aa55089
okawaffles <okawaffles@gmail.com>
2023-03-16 20:07:06 -0500

    more language

--- views/assets/scripts/language.js
+++ views/assets/scripts/language.js
@@ -10,7 +10,8 @@ let jajp = {
             "logout":'ログアウト'
         },
         "banner": {
-            "gdpr":"GDPR COOKIE REGULATION BANNER",
+            "gdpr":"我々は、ログインクッキーためたった。心得ないなら、お願いOkayuCDNを使うません。",
+            "okay":"はい",
             "tfa":"今は、OkayuCDNの二要素認証はベータ版です！",
             "tfa_try":"それ試す！"
         },
@@ -19,6 +20,19 @@ let jajp = {
             "ここで君のファイルを無料にアップロード！",
             "全部なファイルタイプとDiscordの埋め込みは支持します"
         ],
+        "login": [
+            "OkayuCDNへようこそ！",
+            "ログインして準備をしましょう！",
+            "ユーザーネーム",
+            "パスワード",
+            "行くよう！",
+            "アカウントが有しません",
+            "二要素認証",
+            "誤りがありますなら、oka@waffle.moeをコンタクトください。",
+            "二要素認証コード",
+            "不正解の二要素認証コード",
+            "行くよう"
+        ]
     }
 }
 
@@ -44,6 +58,7 @@ function checkLanguage(page) {
             case 'landing':
                 doMenubar(jajp, 'landing');
                 break;
+
             case 'home':
                 doMenubar(jajp, 'home');
                 $('#t_title').html(jajp.parts.home[0])
@@ -51,6 +66,27 @@ function checkLanguage(page) {
                 $('#t_sb').html(jajp.parts.home[2])
                 $('#gdpr')[0].children[0].innerText = jajp.parts.banner.tfa;
                 $('#gdpr')[0].children[1].innerText = jajp.parts.banner.tfa_try;
+                break;
+
+            case 'login':
+                doMenubar(jajp, 'home');
+                $("#gdpr")[0].children[0].innerText = jajp.parts.banner.gdpr;
+                $("#gdpr")[0].children[1].innerText = jajp.parts.banner.okay;
+                $(".login_title").html(jajp.parts.login[0])
+                $(".login_subtitle")[0].innerText = jajp.parts.login[1]
+                $("#un")[0].placeholder = jajp.parts.login[2]
+                $("#pw")[0].placeholder = jajp.parts.login[3]
+                $("#submit").html(jajp.parts.login[4])
+                $(".noacc").html(jajp.parts.login[5])
+                $(".login_subtitle")[1].innerText = jajp.parts.login[6]
+                $(".login_subtitle")[2].innerText = jajp.parts.login[7]
+                $("#code")[0].placeholder = (jajp.parts.login[8])
+                $(".error_2fa").text(jajp.parts.login[9])
+                $("#go_2fa").text(jajp.parts.login[10])
+                break;
+
+            case 'upload':
+                doMenubar(jajp, 'home');
                 break;
         
             default:
