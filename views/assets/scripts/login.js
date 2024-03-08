4eb6463894cf5133c2f2fd66b094133bd5a7f18a
Eli Clark <okawaffles@gmail.com>
2024-03-08 12:38:44 -0600

    merge conflicts will go insane, but so does this animation

--- views/assets/scripts/login.js
+++ views/assets/scripts/login.js
@@ -1,21 +1,36 @@
 let currentProcess = 'login';
-let next = '/';
+let next = '/account';
 if (document.location.toString().includes('?redir=')) {
     next = document.location.toString().split('?redir=')[1];
 }
 
 $(document).ready(() => {
     $('#login').on('click', () => {
+
+        if (currentProcess == '2fa') {
+            $.post('/api/2fa/verify', {username:$('#username')[0].value, userToken:$('#twofactor-code')[0].value})
+            return;
+        }
+
         // hide buttons
         $('#login').css('visibility', 'hidden');
+        $('#username').prop('disabled', true);
+        $('#password').prop('disabled', true);
 
         // send login post
         $.post('/api/login', {username:$('#username')[0].value, password:$('#password')[0].value}, (data) => {
             console.log(data);
-            if (data.result != 200) {
+            if (data.result == 200) {
                 // login failed, show error and let user try again
                 $('#login').css('visibility', 'visible');
                 $('#login_error').css('visibility', 'visible').html('Please check your username and password!');
+                $('#username').prop('disabled', false);
+                $('#password').prop('disabled', false)[0].value = ''; // un-disable AND clear it
+
+                $('#inputs').css('animation', 'bad-login 0.5s ease-in-out');
+                setTimeout(() => {
+                    $('#inputs').css('animation', 'none');
+                }, 550);
                 return;
             }
 
@@ -23,6 +38,11 @@ $(document).ready(() => {
                 document.cookie = `token=${data.token};`;
                 document.location = next;
                 return;
+            } else {
+                currentProcess = '2fa';
+                $('#login').css('visibility', 'visible').html('Verify 2FA');
+                $('#inputs').css('display', 'none');
+                $('#twofactor-inputs').css('display', 'flex');
             }
         });
     });
