63858704838330a55369996b83c3022fe982729a
Lilia Aurora <oka@waffle.moe>
2024-04-29 17:19:33 -0500

    do a couple... funnies...

--- src/routes/simple.ts
+++ src/routes/simple.ts
@@ -2,6 +2,11 @@ import { Request, Response } from 'express';
 import { Router, version } from '../main';
 import { HandleBadRequest, ValidateLoginGET, ValidateToken } from '../util/sanitize';
 import { PrefersLogin } from '../util/secure';
+import { rmSync } from 'node:fs';
+import { TOKEN_DATABASE_PATH } from '../util/paths';
+import { matchedData } from 'express-validator';
+import { join } from 'node:path';
+import { error } from 'okayulogger';
 
 /**
  * These are routes that don't change much, such as /home and /info.
@@ -19,12 +24,22 @@ export function RegisterSimpleRoutes() {
     Router.get('/test', (req: Request, res: Response) => {
         if (req.query.invokeError == '400') return res.status(400).render('err400.ejs');
         if (req.query.invokeError == '500') return res.status(500).render('err500.ejs');
+        if (req.query.aprilfools == 'true') return res.render('assets/aprilfools/home', {version:'TEST PAGE'});
 
         res.render('test.ejs');
     });
 
     Router.get('/login', ValidateLoginGET(), HandleBadRequest, (req: Request, res: Response) => {
         res.render('login.ejs');
+    });
+    Router.get('/logout', ValidateToken(), PrefersLogin, (req: Request, res: Response) => {
+        const data = matchedData(req);
+        res.cookie('token', 'logout').redirect('/login');
+        try {   
+            rmSync(join(TOKEN_DATABASE_PATH, `${data.token}.json`));
+        } catch(err: unknown) {
+            error('logout', <string> err);
+        }
     });
 
     Router.get('/upload', ValidateToken(), PrefersLogin, HandleBadRequest, (req: Request, res: Response) => {
