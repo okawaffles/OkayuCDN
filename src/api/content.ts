e6b9b2e5f542cb013588b0a37eb1e153d959c34c
Lilia Aurora <oka@waffle.moe>
2024-05-28 18:35:32 -0500

    delete remnants of unsuccessful file uploads on content get

--- src/api/content.ts
+++ src/api/content.ts
@@ -1,7 +1,7 @@
 import { join } from 'node:path';
 import { ContentItem, StorageData, UserModel } from '../types';
 import { UPLOADS_PATH } from '../util/paths';
-import { readdirSync, statSync } from 'node:fs';
+import { readdirSync, rmSync, statSync } from 'node:fs';
 import { GetProtectedFiles } from '../util/secure';
 
 
@@ -15,8 +15,16 @@ export function GetStorageInfo(user: UserModel): StorageData {
     readdirSync(contentPath).forEach((name: string) => {
         // read each file and get its size
         const size: number = statSync(join(contentPath, name)).size;
-        usedStorage += size;
-        content.push({ name, size });
+
+        // Check if user has remnants of a broken file upload
+        // if so, remove them, and don't add them to the user's storage
+        if (name.startsWith('LATEST.UPLOADING')) {
+            rmSync(join(contentPath, name));
+        } else {
+            usedStorage += size;
+            content.push({ name, size });
+        }
+
     });
 
     const protected_files: Array<string> = GetProtectedFiles(user.username);
