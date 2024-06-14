35cbbfd3455b5bbf4091950d493fc0f2dc9db1f5
Lilia Aurora <oka@waffle.moe>
2024-06-14 01:10:18 -0500

    now is the perfect time to implement this :3

--- src/util/requestinfo.ts
+++ src/util/requestinfo.ts
@@ -3,14 +3,16 @@ import { Router } from '../main';
 import { Logger } from 'okayulogger';
 import { red, green, blue, bold } from 'chalk';
 //import { readFileSync } from 'node:fs';
-//import { DATABASE_PATH } from './paths';
+import { DATABASE_PATH } from './paths';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 const L: Logger = new Logger('RequestInfo');
 
-//const BannedIPs: Array<string> = JSON.parse(readFileSync(DATABASE_PATH, ''));
+const BannedIPs: Array<string> = JSON.parse(readFileSync(join(DATABASE_PATH, 'banned_ips.json'), 'utf-8')).banned;
 
 export function RegisterRequestLogger(): void {
-    Router.use('*', (req: Request, _res: Response, next: CallableFunction) => {
+    Router.use('*', (req: Request, res: Response, next: CallableFunction) => {
         let IPAddress: string = 'IP Unavailable';
         
         if (req.headers && req.headers['x-forwarded-for'])
@@ -18,7 +20,7 @@ export function RegisterRequestLogger(): void {
         if (req.socket.remoteAddress)
             IPAddress = req.socket.remoteAddress;
 
-
+        if (BannedIPs.indexOf(IPAddress) != -1) return res.status(403).render('err403');
 
         L.info(`${bold(red(IPAddress))} ${blue(req.method)} ${green(req.originalUrl)}`);
 
