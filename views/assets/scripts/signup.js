9bf737282259938afcfd64c0c48853c0df578247
Lilia Aurora <oka@waffle.moe>
2024-07-19 23:11:34 -0500

    start working on new signup page

--- views/assets/scripts/signup.js
+++ views/assets/scripts/signup.js
@@ -1,12 +1,14 @@
+const REGEX_USERNAME = new RegExp('^[a-zA-Z0-9]{6,25}$');
+const REGEX_PASSWORD = new RegExp('^(?=.*[a-z])(?=.*[A-Z].*[A-Z])(?=.*\\d.*\\d)(?=.*[^a-zA-Z\\d].*[^a-zA-Z\\d]).{8,}$');
+const REGEX_EMAIL = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$');
+
 $(document).ready(() => {
     $('#submit').on('click', () => {
-        const usernameRegex = new RegExp('^[a-zA-Z0-9]{6,25}$');
-        const passwordRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z].*[A-Z])(?=.*\\d.*\\d)(?=.*[^a-zA-Z\\d].*[^a-zA-Z\\d]).{8,}$');
-        const emailRegex = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$');
+        $('#submit').css('display', 'none');
 
-        if (!usernameRegex.test($('#username').val())) return alert('Please follow the requested username format');
-        if (!passwordRegex.test($('#password').val())) return alert('Please follow the requested password format');
-        if (!emailRegex.test($('#email').val())) return alert('Please follow the requested email format');
+        if (!REGEX_USERNAME.test($('#username').val())) return alert('Please follow the requested username format');
+        if (!REGEX_PASSWORD.test($('#password').val())) return alert('Please follow the requested password format');
+        if (!REGEX_EMAIL.test($('#email').val())) return alert('Please follow the requested email format');
 
         $.post('/api/signup', {
             username:$('#username').val(),
@@ -18,6 +20,32 @@ $(document).ready(() => {
             document.location = '/login';
         }).fail(() => {
             alert('Something went wrong while creating your account. Please try again.');
+            $('#submit').css('display', 'revert');
         });
+    });
+
+    $('#username').focusout(() => {
+        if (!REGEX_USERNAME.test($('#username').val())) {
+            $('#username').css('animation', 'bad-login 0.5s ease');
+            setTimeout(() => {
+                $('#username').css('animation', 'none');
+            }, 550);
+        }
+    });
+    $('#password').focusout(() => {
+        if (!REGEX_PASSWORD.test($('#password').val())) {
+            $('#password').css('animation', 'bad-login 0.5s ease');
+            setTimeout(() => {
+                $('#password').css('animation', 'none');
+            }, 550);
+        }
+    });
+    $('#email').focusout(() => {
+        if (!REGEX_PASSWORD.test($('#email').val())) {
+            $('#email').css('animation', 'bad-login 0.5s ease');
+            setTimeout(() => {
+                $('#email').css('animation', 'none');
+            }, 550);
+        }
     });
 });
