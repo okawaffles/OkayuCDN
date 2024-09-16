4a7a8d3f419dcac934d946658bcbe0ae863c30be
Lilia Aurora <oka@waffle.moe>
2024-09-16 17:22:58 -0500

    new experiments and bugfixes dropped

--- views/assets/scripts/mybox_v6.js
+++ views/assets/scripts/mybox_v6.js
@@ -3,6 +3,8 @@ let USERNAME = 'anonymous';
 let BOX_ITEMS = [];
 let PROTECTED_BOX_ITEMS = [];
 let USED_STORAGE, TOTAL_STORAGE;
+const EXPERIMENT_ITEMS_HOVER = document.cookie.includes('okayu-experiment=mybox-items-hover');
+const EXPERIMENT_ITEMS_SMOOTH = document.cookie.includes('okayu-experiment=mybox-items-smooth') || EXPERIMENT_ITEMS_HOVER; // must be on for hover experiment to work
 const IS_MOBILE = navigator.userAgent.includes('Android') || navigator.userAgent.includes('iOS');
 
 $(document).ready(() => {
@@ -20,6 +22,8 @@ $(document).ready(() => {
         $('#content_container').empty();
         RenderBox();
     });
+
+    if (EXPERIMENT_ITEMS_SMOOTH || EXPERIMENT_ITEMS_HOVER) $('#experiment-on').css('display', 'inline');
 });
 
 
@@ -109,13 +113,40 @@ function AddItem(item, id, private) {
 
     alternate = !alternate;
 
-    $('#content_container').append(element);
+    if (EXPERIMENT_ITEMS_HOVER) {
+        $('#content_container').append(element);
+        $(`#item-${id}`).hover(() => {
+            dropdown(id);
+        }, () => {
+            dropdown(id);
+        });
+    } else $('#content_container').append(element);
 }
 
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function dropdown(id) {
     const item = id.toString();
+
+    if (EXPERIMENT_ITEMS_SMOOTH) {
+        if ($(`#showhide-id-${id}`).css('height') == '0px') {
+            console.log('experiment item open');
+
+            let height = $(`#showhide-id-${id}`)[0].scrollHeight + 15 + 'px';
+
+            $(`#showhide-id-${item}`).css('height', height);
+            $(`#showhide-id-${item}`).css('padding-top', '5px');
+            $(`#showhide-button-${item}`).html('<i class="fa-solid fa-caret-up"></i>');
+        } else {
+            console.log('experiment item closed');
+
+            $(`#showhide-id-${item}`).css('height', '0px');
+            $(`#showhide-id-${item}`).css('padding-top', '0');
+            $(`#showhide-button-${item}`).html('<i class="fa-solid fa-caret-down"></i>');
+        }
+        
+        return;
+    }
 
     if ($(`#showhide-id-${item}`).css('display') == 'none') {
         $(`#showhide-id-${item}`).css('display', 'flex');
@@ -162,7 +193,7 @@ function generateItem(id, item, fsize, alternate, private) {
             </button>
         </div>
     </div>
-    <div class="bottom" id="showhide-id-${id}">
+    <div class="bottom ${EXPERIMENT_ITEMS_SMOOTH?'experiment-mybox-items-smooth':''}" id="showhide-id-${id}">
         <button class="share desktop" id="share-content-${id}" onclick="share('${item}', ${id}, false)"><i class="fa-solid fa-arrow-up-right-from-square"></i> Share</button>
         <button class="view desktop" onclick="view('${item}')"><i class="fa-solid fa-eye"></i> View</button>
         <button class="dl desktop" onclick="download('${item}')"><i class="fa-solid fa-download"></i> Download</button>
