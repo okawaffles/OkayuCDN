1af24e18b2cb33da1ad496c30f9982b70f85550e
Lilia Aurora <oka@waffle.moe>
2024-11-22 16:39:07 -0600

    language poc test

--- views/assets/scripts/language.js
+++ views/assets/scripts/language.js
@@ -1,6 +1,21 @@
 const LANG_JP = {
+    '_':{ // these will execute on all pages
+        'NAV_HOME':'<i class="fa-solid fa-house"></i> トップ',
+        'NAV_UPLOAD':'<i class="fa-solid fa-upload"></i> アップロード',
+        'NAV_BOX':'<i class="fa-solid fa-box"></i> ダンボール',
+        'NAV_INFO':'<i class="fa-solid fa-circle-info"></i> インフォ',
+        'NAV_ACCOUNT':'<i class="fa-solid fa-user"></i> マイページ',
+        'NAV_M_HOME':'<i class="fa-solid fa-house"></i> トップ',
+        'NAV_M_UPLOAD':'<i class="fa-solid fa-upload"></i> アップロード',
+        'NAV_M_BOX':'<i class="fa-solid fa-box"></i> ダンボール',
+        'NAV_M_INFO':'<i class="fa-solid fa-circle-info"></i> インフォ',
+        'NAV_M_ACCOUNT':'<i class="fa-solid fa-user"></i> マイページ',
+        'NAV_M_MENU':'<i class="fa-solid fa-bars"></i> メニュー'
+    },
     '/home': {
-        'WELCOME': 'OkayuCDNへようこそ！'
+        'WELCOME': 'OkayuCDNへようこそ！',
+        'SUB_A':'ここは君のファイルを無料アップロードできる',
+        'SUB_B':'ではどこなりとシェアできます！'
     }
 };
 const LANG_ES = {
@@ -25,6 +40,9 @@ jQuery(() => {
         break;
     }
 
+    Object.keys(LANG['_']).forEach(rule => {
+        $(`.__LANG_${rule}`).html(LANG['_'][rule]);
+    });
     Object.keys(LANG[PAGE]).forEach(rule => {
         $(`.__LANG_${rule}`).text(LANG[PAGE][rule]);
     });
