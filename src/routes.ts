65dd5718d1e92351cf66646f45401864533ba48f
Millie <oka@waffle.moe>
2025-09-27 23:48:40 -0500

    remove goldenmira landing route

--- src/routes.ts
+++ src/routes.ts
@@ -21,9 +21,6 @@ export function RegisterRoutes() {
     Router.get('/wallpaper', (req: Request, res: Response) => {
         res.render('landing/okayu_wallpaper.ejs', {domain}); // this may need to be changed later
     });
-    Router.get('/mira', (req: Request, res: Response) => {
-        res.render('landing/mira.ejs'); // this may need to be changed later
-    });
     Router.get('/korone', (req: Request, res: Response) => {
         res.render('landing/korone.ejs'); // this may need to be changed later
     });
